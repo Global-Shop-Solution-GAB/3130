f39ac6ae5b7a4fc329d33a309ba1ca7f96ea2070
Service Web <sw@gssmail.com>
2024-03-03 21:16:44 -0600

    Commit changes in C:\Websites\Git\emarlow\3130\GCG_3130_PL_Discount.sql

---
+++ GCG_3130_PL_Discount.sql
@@ -0,0 +1,5 @@
+CREATE TABLE "GCG_3130_PL_Discount"(
+ "Price_Class" CHAR(1) NOT NULL ,
+ "Product_Line" CHAR(2) NOT NULL ,
+ "Discount" NUMERIC(7,4) NOT NULL,
+ "Grid_Location" CHAR(10) NOT NULL );
